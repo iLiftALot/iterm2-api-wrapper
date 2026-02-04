92a7bc11c42975be450f403cad9e0ff347a4f5e7
Nick Corbin <nickcorbin17@yahoo.com>
2026-02-03 21:54:54 -0500

    refactor: organize imports and define __all__ for public API exposure

--- src/iterm2_api_wrapper/__init__.py
+++ src/iterm2_api_wrapper/__init__.py
@@ -1,5 +1,12 @@
 """Top-level package for Iterm2 Scripts."""
 
+from .client import create_iterm_client
+
 __package__ = "iterm2_api_wrapper"
 __author__ = "Nicholas Corbin"
 __email__ = "nickcorbin17@yahoo.com"
+
+
+__all__ = [
+    "create_iterm_client",
+]
